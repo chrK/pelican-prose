7f827e9721fc2ce808f8bab05c1a566676a95a35
Christian Mueller <christian@version2.io>
2013-02-17 18:36:57 +0100

    Initial commit
    First typographic styling done.

---
+++ templates/impressum.php
@@ -0,0 +1,107 @@
+<?php
+/*
+Template Name: impressum
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="heading">			
+		<h2>Impressum</h2>
+</div>
+	
+
+<div class="column span-8 prepend-3">
+	<div class="entry">
+
+								
+		<div id="impressum-1"></div>
+		<p><script type="text/javascript">
+//<![CDATA[
+
+function hiveware_enkoder(){var i,j,x,y,x=
+"x=\"783d22312a3478653d645c226f34433672393467676e3530337a383e6167683843376d" +
+"386f66686b343d6532353f656b723866372e3967356e38382a3b7438713368383d69687238" +
+"743653383d2466293629373f387b323d2b3f797a3b5c5c345c22395c5c345c5c333d38783b" +
+"2438643d352b326a3529773252387d69353b3f30353d5c5c695c22283d72786f3866753b30" +
+"277a272d3d2979373b3829343139283a72277429732a6267757273382e3278343d3378383b" +
+"6329652975306728707465413572346134683a63772e3f782d287b6c7d6134763765383d36" +
+"6a393b2b5c5c345c223f792d7d6b293b6a382834643935393b3d316a3d762b6969703b3968" +
+"67746367736e6565366c392e6778383c68696e3b75353d3d78693b283872646f3866637d35" +
+"3b272927353d2c79693b28687238743673396265755c5c735c222e5c5c785c5c3d7b2b2179" +
+"3d7b2b2938303b31383d332b38692b3b34682e746b672a6e6665386c342e3478653c743b76" +
+"29756a6428777237746673356238752b73672e3479363d3979657d693b3b296835742c6769" +
+"382837723874397339626e7565736c2e2e78783d342b3979377b382936303c5c22693b3b79" +
+"303d3d273927673b38663b6f387269282869723d6f30663b3a69383c3278362e366c3b6529" +
+"6e786728746568393b6469382b333d383570386129397b3266346f367233283c6a6a3d284d" +
+"666139746468382e336d3869696e3b2832782d2e296c3665396e676738746868692c286974" +
+"2b413565383829643b362d672d356a643e6f3d4369723b6129347b3479652b373d3678682e" +
+"63632e6878613d72394137743828676a38296a3b7b7d297d2b79343b223b6a3d6576616c28" +
+"782e636861724174283029293b783d782e7375627374722831293b793d27273b666f722869" +
+"3d303b693c782e6c656e6774683b692b3d32297b792b3d782e73756273747228692c31293b" +
+"7d666f7228693d313b693c782e6c656e6774683b692b3d32297b792b3d782e737562737472" +
+"28692c31293b7d793d792e737562737472286a293b\";y='';for(i=0;i<x.length;i+=2)" +
+"{y+=unescape('%'+x.substr(i,2));}y";
+while(x=eval(x));}hiveware_enkoder();
+
+//]]>
+</script><br />
+		Internet: <a href="http://hinterwelt.net/">http://hinterwelt.net</a></p>
+
+		<div id="impressum-2"></div>
+</div>
+</div>
+<div class="heading">			
+		<h2>Disclaimer &mdash; rechtliche Hinweise<a name="disclaimer"></a></h2>
+</div>
+	
+
+<div class="column span-8 prepend-3">
+	<div class="entry">
+
+
+
+<h3>(1) Haftungsbeschränkung</h3>
+
+
+<h4>Inhalte dieser Website</h4>
+
+<p>Die Inhalte dieser Website werden mit größtmöglicher Sorgfalt erstellt. Der Anbieter übernimmt jedoch keine Gewähr für die Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten Inhalte. Die Nutzung der abrufbaren Inhalte erfolgt auf eigene Gefahr des Nutzers. Namentlich gekennzeichnete Beiträge, wie beispielsweise in den Kommentaren, geben die Meinung des jeweiligen Autors und nicht immer die Meinung des Anbieters wieder.</p>
+
+<h4>Externe Links</h4>
+
+<p>Diese Website enthält Verknüpfungen zu Websites Dritter ("externe Links"). Diese Websites unterliegen der Haftung der jeweiligen Betreiber. Der Anbieter hat bei der erstmaligen Verknüpfung der externen Links die fremden Inhalte daraufhin überprüft, ob etwaige Rechtsverstöße bestehen. Zu dem Zeitpunkt waren keine Rechtsverstöße ersichtlich. Der Anbieter hat keinerlei Einfluss auf die aktuelle und zukünftige Gestaltung und auf die Inhalte der verknüpften Seiten. Das Setzen von externen Links bedeutet nicht, dass sich der Anbieter die hinter dem Verweis oder Link liegenden Inhalte zu Eigen macht. Eine ständige Kontrolle dieser externen Links ist für den Anbieter ohne konkrete Hinweise auf Rechtsverstöße nicht zumutbar. Bei Kenntnis von Rechtsverstößen werden jedoch derartige externe Links unverzüglich gelöscht.</p>
+
+<h4>Kein Vertragsverhältnis</h4>
+
+<p>Mit der Nutzung der Website des Anbieters kommt keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande. Insofern ergeben sich auch keinerlei vertragliche oder quasivertragliche Ansprüche gegen den Anbieter.</p>
+
+<p>Für den Fall, dass die Nutzung der Website doch zu einem Vertragsverhältnis führen sollte, gilt rein vorsorglich nachfolgende Haftungsbeschränkung: Der Anbieter haftet für Vorsatz und grobe Fahrlässigkeit sowie bei Verletzung einer wesentlichen Vertragspflicht (Kardinalpflicht). Der Anbieter haftet unter Begrenzung auf Ersatz des bei Vertragsschluss vorhersehbaren vertragstypischen Schadens für solche Schäden, die auf einer leicht fahrlässigen Verletzung von Kardinalpflichten durch ihn oder eines seiner gesetzlichen Vertreter oder Erfüllungsgehilfen beruhen. Bei leicht fahrlässiger Verletzung von Nebenpflichten, die keine Kardinalpflichten sind, haftet der Anbieter nicht. Die Haftung für Schäden, die in den Schutzbereich einer vom Anbieter gegebenen Garantie oder Zusicherung fallen sowie die Haftung für Ansprüche aufgrund des Produkthaftungsgesetzes und Schäden aus der Verletzung des Lebens, des Körpers oder der Gesundheit bleibt hiervon unberührt.</p>
+
+<h3>(2) Urheber- und Kennzeichenrecht</h3>
+
+<p>Die AutorInnen sind bestrebt, in allen Publikationen die Urheberrechte der verwendeten Grafiken, Tondokumente, Videosequenzen und Texte zu beachten, von ihnen selbst erstellte Grafiken, Tondokumente, Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken, Tondokumente, Videosequenzen und Texte zurückzugreifen. Alle innerhalb des Internetangebotes genannten und ggf. durch Dritte geschützten Marken- und Warenzeichen unterliegen uneingeschränkt den Bestimmungen des jeweils gültigen Kennzeichenrechts und den Besitzrechten der jeweiligen eingetragenen Eigentümer. Allein aufgrund der bloßen Nennung ist nicht der Schluss zu ziehen, dass Markenzeichen nicht durch Rechte Dritter geschützt sind! Sofern nicht anders gekennzeichnet, stehen alle Artikel dieses Weblogs unter einer <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.0/de/">Creative Commons Lizenz</a>.</p>
+
+<p>Links zur Website des Anbieters sind jederzeit willkommen und bedürfen keiner Zustimmung durch den Anbieter der Website.</p>
+
+<h3>(3) Datenschutz</h3>
+
+<p>Durch den Besuch der Website des Anbieters können Informationen über den Zugriff (Datum, Uhrzeit, betrachtete Seite) auf dem Server gespeichert werden. Diese Daten gehören nicht zu den personenbezogenen Daten, sondern sind anonymisiert. Sie werden ausschließlich zu statistischen Zwecken ausgewertet. Eine Weitergabe an Dritte, zu kommerziellen oder nichtkommerziellen Zwecken, findet nicht statt.</p>
+
+<p>Des Weiteren können Daten auf dem Computer der Websitenutzer gespeichert werden. Diese Daten nennt man "Cookie", die dazu dienen, das Zugriffsverhalten der Nutzer zu vereinfachen. Der Nutzer hat jedoch die Möglichkeit, diese Funktion innerhalb des jeweiligen Webbrowsers zu deaktivieren. In diesem Fall kann es jedoch zu Einschränkungen der Bedienbarkeit unserer Website kommen.</p>
+
+<p>Der Anbieter weist ausdrücklich darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen und nicht lückenlos vor dem Zugriff durch Dritte geschützt werden kann.</p>
+
+<p>Die Verwendung der Kontaktdaten der Anbieterkennzeichnung zur gewerblichen Werbung ist ausdrücklich nicht erwünscht, es sei denn der Anbieter hatte zuvor seine schriftliche Einwilligung erteilt oder es besteht bereits ein geschäftlicher Kontakt. Der Anbieter und alle auf dieser Website genannten Personen widersprechen hiermit jeder kommerziellen Verwendung und Weitergabe ihrer Daten.</p>
+
+<h3>(4) Anwendbares Recht</h3>
+
+<p>Es gilt ausschließlich das maßgebliche Recht der Bundesrepublik Deutschland.</p>
+
+<p>Nach: <a href="http://www.juraforum.de/disclaimer_muster/">Jura-Forum.de</a></p>
+
+
+</div>
+</div>
+<div class="clear"></div>
+<?php get_footer(); ?>
